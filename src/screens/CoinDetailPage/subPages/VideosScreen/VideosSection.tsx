a92cd07d84e6c8a52276e218de96c2bb259be179
aranav1999@gmail.com <aranav1999@gmail.com>
2025-01-30 11:55:41 +0530

    Coin Screen added

---
+++ src/screens/CoinDetailPage/subPages/VideosScreen/VideosSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const VideosScreen = () => {
+  return (
+    <View>
+      <Text>Videos Section</Text>
+    </View>
+  )
+}
