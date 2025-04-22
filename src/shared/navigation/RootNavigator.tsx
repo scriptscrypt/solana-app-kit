fddbbe7ae7692057ecd2ec5ddaff9828991a15ab
aranav1999@gmail.com <aranav1999@gmail.com>
2025-04-22 19:50:49 +0530

    LaunchLab design added

--- src/shared/navigation/RootNavigator.tsx
+++ src/shared/navigation/RootNavigator.tsx
@@ -7,6 +7,7 @@ import CoinDetailPage from '@/screens/SampleUI/Threads/CoinDetailPage/CoinDetail
 import { PumpfunScreen, PumpSwapScreen } from '@/modules/pumpFun';
 import { TokenMillScreen } from '@/modules/tokenMill';
 import { NftScreen } from '@/modules/nft';
+import LaunchlabsScreen from '@/modules/LaunchLab/screens/LaunchlabsScreen';
 import ChatScreen from '@/screens/SampleUI/Chat/ChatScreen/ChatScreen';
 import ChatListScreen from '@/screens/SampleUI/Chat/ChatListScreen';
 import OtherProfileScreen from '@/screens/SampleUI/Threads/OtherProfileScreen/OtherProfileScreen';
@@ -34,6 +35,7 @@ export type RootStackParamList = {
   };
   PumpSwap: undefined;
   MercuroScreen: undefined;
+  LaunchlabsScreen: undefined;
   // NEW ROUTE
   OtherProfile: { userId: string };
   PostThread: { postId: string };
@@ -63,6 +65,7 @@ export default function RootNavigator() {
           <Stack.Screen name="ChatScreen" component={ChatScreen} />
           <Stack.Screen name="PumpSwap" component={PumpSwapScreen} />
           <Stack.Screen name="MercuroScreen" component={MercuroScreen} />
+          <Stack.Screen name="LaunchlabsScreen" component={LaunchlabsScreen} />
 
           {/* NEW SCREEN for viewing other user's profile */}
           <Stack.Screen name="OtherProfile" component={OtherProfileScreen} />
